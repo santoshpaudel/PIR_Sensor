241e8bd97a8c965ca4e1bb65c009711cfb96cf1d
santoshpaudel <spaudel@villanova.edu>
2018-04-11 17:27:25 -0400

    Arduino code

---
+++ sketch_mar27b.ino
@@ -0,0 +1,44 @@
+//define the pins
+int LED = 13;
+int PIR = 7;
+//int Buzzer = 2;
+int minSecsBetweenEmails = 60; // 1 min
+ 
+long lastSend = -minSecsBetweenEmails * 1000l;
+
+void setup() {
+  //define the LED and Buzzer pin as output
+  pinMode(LED, OUTPUT);
+  //pinMode(Buzzer, OUTPUT);
+  //define the sensor pin as input
+  pinMode(PIR, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  //using the digitalRead function we will read the signal of the sensor
+  int value = digitalRead(PIR);
+   long now = millis();
+  //if its high or if an any object is detected it will activate the LED 
+  if (value == HIGH){
+    digitalWrite(LED, HIGH);
+    if (now > (lastSend + minSecsBetweenEmails * 1000l))
+    {
+      Serial.println("MOVEMENT");
+      lastSend = now;
+    }
+    else
+    {
+      Serial.println("Wait for few seconds");
+    }
+  }
+  
+ 
+   // digitalWrite(Buzzer, HIGH);
+  
+  else {
+    digitalWrite(LED, LOW);
+    //digitalWrite(Buzzer, LOW);
+  }
+   delay(500);
+}
